bd84ee509307c62816ba0f7d42899def51aa652d
Dylan Desmond <Dylan Desmond>
2016-10-18 15:12:27 -0700

    Completed exercise 3 besides nested and challenge

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -4,21 +4,27 @@
 
 # Install devtools package: allows installations from GitHub
 install.packages('devtools')
+install.packages('dplyr')
 
 # Install "fueleconomy" package from GitHub
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
-
+library(fueleconomy)
+library(dplyr)
+View(vehicles)
 # Which Accura model has the best hwy MPG in 2015? (without method chaining)
-
+acuras <- vehicles[vehicles$make == 'Acura',]
+acuras.2015 <- acuras[acuras$year == 2015,]
+acura.highest.hwy.2015 <- acuras.2015[acuras.2015$hwy == max(acuras.2015$hwy), 'model']
 
 # Which Accura model has the best hwy MPG in 2015? (nesting functions)
 
-
 # Which Accura model has the best hwy MPG in 2015? (pipe operator)
-
+acura.highest.hwy.2015.pipe <- filter(vehicles, make == 'Acura') %>%
+                                  filter(year == 2015) %>%
+                                  filter(hwy == max(hwy)) %>%
+                                  select(model)
 
 ### Bonus ###
 
